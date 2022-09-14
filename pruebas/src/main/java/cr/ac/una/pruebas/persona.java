8a5269515a5e9cbcd93c2f1277b0c77020175d24
pedroveg <pedro.vega.rojas@est.una.ac.cr>
2022-09-14 17:00:52 -0600

    primer commit

---
+++ pruebas/src/main/java/cr/ac/una/pruebas/persona.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.pruebas;
+
+/**
+ *
+ * @author leon vega
+ */
+public class persona {
+    
+    private String nombre;
+    
+}
